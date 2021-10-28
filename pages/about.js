3324795c8b6de178c8b88668bb755e8976f274e8
Rimantas <belauzas@yahoo.com>
2021-10-28 19:49:55 +0300

    Dinamic pages rendering with JS

---
+++ pages/about.js
@@ -0,0 +1,42 @@
+function pageAbout() {
+    return `<!DOCTYPE html>
+            <html lang="en">
+
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Receptai</title>
+
+                <base href="http://localhost:3000/">
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+                <link rel="manifest" href="/site.webmanifest">
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+                <meta name="msapplication-TileColor" content="#da532c">
+                <meta name="theme-color" content="#ffffff">
+
+                <link rel="stylesheet" href="/css/reset.css">
+                <link rel="stylesheet" href="/css/main.css">
+                <link rel="stylesheet" href="/css/components/button.css">
+            </head>
+
+            <body>
+                <h1>Receptai - apie mus</h1>
+                <p>Cia bus patys blogiausi receptai pasaulyje - uzsukite kai bus paruosta 😈🍕</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor fugiat sint, tenetur dolore fugit quo! Consectetur maxime incidunt cumque impedit ut amet fugit, odio exercitationem eius, tempora ratione consequatur minima.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor fugiat sint, tenetur dolore fugit quo! Consectetur maxime incidunt cumque impedit ut amet fugit, odio exercitationem eius, tempora ratione consequatur minima.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor fugiat sint, tenetur dolore fugit quo! Consectetur maxime incidunt cumque impedit ut amet fugit, odio exercitationem eius, tempora ratione consequatur minima.</p>
+
+                <img src="/img/saliami-1.jpg" alt="Saliamis">
+                <img src="/img/vaikiska-pica.png" alt="Vaikiska pica">
+                <script src="/js/main.js"></script>
+            </body>
+
+            </html>`;
+}
+
+module.exports = pageAbout;
